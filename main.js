02dd5e45130ac4e0b86d28df42a71f22c918e4c6
Bony <gubonhy@naver.com>
2021-08-14 19:40:05 +0900

    skill

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const tabList = document.querySelectorAll(".navbar__menu li");
 const content = document.querySelectorAll(".contents .content");
 let active = "";
 
-console.log("fini");
 for (let i = 0; i < tabList.length; i++) {
   tabList[i].querySelector(".btn").addEventListener("click", function (e) {
     if (document.readyState === "complete") {
